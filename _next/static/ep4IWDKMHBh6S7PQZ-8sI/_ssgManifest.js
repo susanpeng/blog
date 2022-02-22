d347f63beca248c85951d4052dc32311fc7c6441
susanpeng <pengyl@email.com>
2022-02-22 14:53:44 -0800

    change writings page

---
+++ _next/static/ep4IWDKMHBh6S7PQZ-8sI/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fsamples","\u002F","\u002Fapidocs","\u002Fsample\u002F[slug]","\u002Fapidoc\u002F[slug]","\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
